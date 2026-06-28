f201a879d016a92d4e05e42aad815364cd318916
beru <beru@beru.com>
2026-06-28 21:11:43 +0000

    device: Complete LineageOS device tree from stock ROM dump
    - Init files (mt6789, usb, x6886) from vendor partition
    - Config files (audio, media, wifi) from vendor partition
    - SELinux policies from vendor partition
    - Prebuilt kernel and DTB from boot.img
    - System and vendor properties from build.prop
    - Proper BoardConfig.mk with correct partition sizes
    - Build fingerprint from stock system/build.prop
    - Branch: lineage-23.2

--- device.mk
+++ device.mk
@@ -1,34 +1,46 @@
 # device.mk - Infinix Hot 60 Pro Plus (X6886)
+# Auto-generated from stock ROM dump
 
 LOCAL_PATH := device/infinix/x6886
 
-# Inherit from vendor tree
+# Inherit from vendor tree (contains proprietary blobs)
 $(call inherit-product, vendor/infinix/X6886/vendor.mk)
 
-# Device identifier
+# Device identifiers
 PRODUCT_DEVICE := x6886
 PRODUCT_NAME := x6886
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix Hot 60 Pro Plus
 PRODUCT_MANUFACTURER := Infinix Mobility Limited
 
-# Screen
 PRODUCT_CHARACTERISTICS := phone
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Common Overlays
+# Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Init Files
+# Init files from stock ROM dump
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.x6886.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.x6886.rc \
     $(LOCAL_PATH)/rootdir/etc/init.mt6789.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.mt6789.rc \
     $(LOCAL_PATH)/rootdir/etc/init.mt6789.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.mt6789.usb.rc \
-    $(LOCAL_PATH)/rootdir/etc/ueventd.mt6789.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
+    $(LOCAL_PATH)/rootdir/etc/init/hw/init.x6886.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.x6886.rc \
+    $(LOCAL_PATH)/rootdir/etc/ueventd.mt6789.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6789:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.mt6789
 
-# Permissions
+# Config files from stock ROM dump
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/configs/audio_policy.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.xml \
+    $(LOCAL_PATH)/configs/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/configs/bluetooth_audio_policy.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wpa_supplicant_overlay.conf
+
+# Hardware permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
@@ -49,38 +61,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.wifi.xml
 
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.xml
-
-# WiFi
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf
-
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/bt_vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
-
 # System properties
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.com.android.dataroaming=false \
     persist.vendor.radio.apm_sim_not_pwdn=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.product.device=x6886 \
-    ro.build.display.id=XOS.15.1.0 \
-    ro.build.version.sdk=34 \
-    ro.build.version.release=15 \
-    ro.build.version.security_patch=2024-05-01 \
-    ro.product.board=mt6789 \
-    ro.hardware=mt6789
-
-# Build fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="Infinix_Hot_60_Pro_Plus-v15-2024-07-01"
-BUILD_FINGERPRINT := Infinix/Infinix_Hot_60_Pro_Plus/X6886:15/UP1A.231005.007/1719849270:user/release-keys
--- lineage_x6886.mk
+++ lineage_x6886.mk
@@ -1,14 +1,13 @@
-# Inherit from common configuration
+# lineage_x6886.mk - LineageOS 23.2 for Infinix Hot 60 Pro Plus (X6886)
+# Auto-generated from stock ROM dump
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from device
 $(call inherit-product, device/infinix/x6886/device.mk)
 
-# Inherit from LineageOS
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Device configuration
 PRODUCT_DEVICE := x6886
 PRODUCT_NAME := lineage_x6886
 PRODUCT_BRAND := Infinix
@@ -16,8 +15,7 @@ PRODUCT_MODEL := Infinix Hot 60 Pro Plus
 PRODUCT_MANUFACTURER := Infinix Mobility Limited
 PRODUCT_RELEASE_NAME := Infinix Hot 60 Pro Plus
 
-# Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="Infinix_Hot_60_Pro_Plus-v15-2024-07-01"
+    PRIVATE_BUILD_DESC="sys_tssi_64_armv82_infinix-user 15 AP3A.240905.015.A2 165004 release-keys"
 
-BUILD_FINGERPRINT := Infinix/Infinix_Hot_60_Pro_Plus/X6886:15/UP1A.231005.007/1719849270:user/release-keys
+BUILD_FINGERPRINT := Infinix/Infinix_Hot_60_Pro_Plus/X6886:15/AP3A.240905.015.A2/165004:user/release-keys
